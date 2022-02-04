299a744956f9207c4b9283d78fa35a787495fce5
JhonyCarvajal <jhony150209@gmail.com>
2022-02-03 22:00:36 -0500

    Recibiendo la Imagen y Convirtiendola en Formato A4

--- controllers/imagen.js
+++ controllers/imagen.js
@@ -1,16 +1,36 @@
 const { response, request } = require('express');
+const sharp = require('sharp');
+
+const fs = require('fs')
 
 
-const imagesGet = ( request, response )=>{
+
+const imagesGet = (req=  request,res=  response )=>{
 
 
 }
 
-const imagePost = ( request, response )=>{
+const imagePost = async (req= request, res= response )=>{
 
-    response.json({ 
-        msg: `Hola mundo desde ImagePOst`
-    })
+    try {
+        
+        const imagen = req.file
+        const imageResized= await sharp(imagen.buffer).resize(796, 1123,{
+            fit: 'contain',
+            background: "#fff"
+        }).toBuffer()
+
+
+        fs.writeFileSync('uploads/prueba.png', imageResized)
+        
+        console.log(imageResized);
+        res.json({ 
+            imageResized,
+            msg: `Hola mundo desde ImagePOst`
+        })
+    } catch (error) {
+        
+    }
    
 
 
--- routes/imagen.js
+++ routes/imagen.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const multer  = require('multer')
+const sharp = require('sharp');
 
 const {
     imagesGet,
     imagePost
   } = require('../controllers/imagen.js');
+
+const storageMulter = multer.memoryStorage() 
+const upload = multer({ storage: storageMulter })
+  
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
@@ -14,6 +20,7 @@ const router = Router();
 router.get('/AllImages',imagesGet )
 
 router.post('/images',[
+    upload.single('imagen'),
     validarCampos
 ],imagePost
 )
